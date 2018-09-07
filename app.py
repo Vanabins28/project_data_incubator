ce9abdff4c840a667fcaff5a36de813180366e48
unknown <jkatigba@gmail.com>
2018-09-07 00:00:29 -0400

    flask_bokeh_new4

--- app.py
+++ app.py
@@ -64,6 +64,8 @@ def make_graph():
 #os.system('rm temp.csv')
 
 if __name__ == '__main__':
-  app.run(port=5000)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
+#  app.run(port=5000)
 #  app.run(port=5000,debug=True)
 #  app.run(port=33507)
